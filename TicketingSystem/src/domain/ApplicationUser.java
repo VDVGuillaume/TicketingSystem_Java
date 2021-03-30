fe85ddedbde5f2ffa5e6fff532cfbf28cf9b16bf
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-30 21:39:44 +0200

    Added model for ApplicationUser

--- TicketingSystem/src/domain/ApplicationUser.java
+++ TicketingSystem/src/domain/ApplicationUser.java
@@ -1,18 +1,47 @@
 package domain;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+
+
+
+
 @Entity
+@NamedQueries({
+	
+	@NamedQuery(
+		    name="ApplicationUser.findUserByUserName",
+		    query="SELECT u FROM ApplicationUser u WHERE u.userName = :userName"
+		)	
+	
+})
 @Table(name ="AspNetUsers")
 public class ApplicationUser implements Serializable {
 	
 	@Id	
-	int Id;
+	String id;
+	int clientId;
+	String userName;
+	String email;
+	String passwordHash;
+	String securityStamp;	
+	String concurrencyStamp;
+	Date LockoutEnd;
+	Boolean LockoutEnabled;
 	
+	
+	@Override
+	public String toString() {
+		
+		return String.format("%s-%s",this.userName,this.email);
+	}	
 	
 }
