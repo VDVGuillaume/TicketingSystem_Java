9a1ba5332422a11add0d034a6b77a3d91f492c18
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-04-05 10:01:48 +0200

    Changed startUp class due to merge conflict

--- TicketingSystem/src/main/StartUp.java
+++ TicketingSystem/src/main/StartUp.java
@@ -14,18 +14,14 @@ public class StartUp {
     }
 
     private void run() {
-<<<<<<< HEAD
+
     	var repository = new UserRepository();
     	
     	var user = repository.getUserByUsername("supportmanager");
     	
     	var stophere = true;
-    	/*
-        ConsoleUi console = new ConsoleUi(new TicketController(), new DomainController(), new LoginController());
-=======
-        ConsoleUi console = new ConsoleUi(new TicketController(), new DomainController(), new LoginController(), new ClientController());
->>>>>>> Added create client method & persistence
-        console.run();
-        
+    	
+    	
+
     }
 }
