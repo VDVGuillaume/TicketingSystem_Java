e73965add7783b5b855f32251b98a83a9e09c43e
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-04-25 11:25:48 +0200

    Moved email validation to domain contact

--- TicketingSystem/src/domain/Contact.java
+++ TicketingSystem/src/domain/Contact.java
@@ -1,10 +1,14 @@
 package domain;
 
 import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import exceptions.ValidationException;
 
 @Entity
 public class Contact implements Serializable {
@@ -16,6 +20,10 @@ public class Contact implements Serializable {
 	
 	public Contact(String email, String firstname, String surname) {
 		
+		if(!emailCheck(email)) {
+			throw new ValidationException("Invalid mail");
+		}
+		
 		this.email = email;
 		this.firstname = firstname;
 		this.surname = surname;
@@ -23,6 +31,15 @@ public class Contact implements Serializable {
 	
 	public Contact() {
 		
+	}
+	
+	
+	public boolean emailCheck(String email) {
+		String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+		 
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
 	}
 	
 	@Override
