e52ac22eeb1c99dc40610d4cd83059c7abfad35a
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-04-25 14:59:50 +0200

    Changed client class with properties, problem with serialization. Probably won't implement this 'solution' and start working with observableList

--- TicketingSystem/src/domain/Client.java
+++ TicketingSystem/src/domain/Client.java
@@ -18,6 +18,15 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
 @Entity
 @NamedQueries({
 	
@@ -38,26 +47,24 @@ public class Client implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int id;
-	String name;
+	private final SimpleIntegerProperty id = new SimpleIntegerProperty();;
+	private final SimpleStringProperty name = new SimpleStringProperty();;
 	@OneToOne(cascade = CascadeType.ALL)
-	Address address = new Address();
+	private final SimpleObjectProperty<Address> address = new SimpleObjectProperty<Address>();
 	@OneToMany(cascade = CascadeType.ALL)
 	@ElementCollection
-	ArrayList<String> telephoneNumbers = new ArrayList<String>();
+	private final SimpleListProperty<String> telephoneNumbers = new SimpleListProperty<String>();
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name="clientID")
-	ArrayList<Contact> contacts = new ArrayList<Contact>();
-	Date dateCreated;
+	private final SimpleListProperty<Contact> contacts = new SimpleListProperty<Contact>();
+	private final SimpleObjectProperty<Date> dateCreated = new SimpleObjectProperty<Date>();
 	
 	public Client(String name,String email,String firstname, String surname, String telephoneNumber,
 			String street,int housenumber, String city, String country,int postalcode) {
 		
 		setName(name);
-		setAddress(new Address(street, housenumber,city,country,postalcode ));	
-		contacts = new ArrayList<Contact>();
-		telephoneNumbers = new ArrayList<String>();
-		dateCreated = new Date();		
+		setAddress(street, housenumber,city,country,postalcode);	
+		setDateCreated(new Date()); 		
 		contacts.add(new Contact(email,firstname,surname));
 		telephoneNumbers.add(telephoneNumber);
 	}
@@ -66,44 +73,78 @@ public class Client implements Serializable {
 		
 	}
 	
-	public void addPhoneNumber(String telephoneNumber){
-		this.telephoneNumbers.add(telephoneNumber);
-	}
-	
-	public void addContact(String email, String firstname, String surname) {
-		contacts.add(new Contact(email,firstname,surname));
-		
-	}
+
 
 	public String getName() {
-		return name;
-	}
-
-	public Date getDateCreated() {
-		return dateCreated;
+		return name.get();
 	}
 	
 	public void setName(String name) {
-		this.name = name;
+		this.name.set(name);
+	}
+	
+	public StringProperty nameProperty() {
+		return name;		
 	}
 	
 	public int getId() {
-		return id;
+		return id.get();
 	}
-
+			
+	public SimpleIntegerProperty idProperty() {
+		return id;		
+	}
+	
 	public Address getAddress() {
-		return address;
+		return address.get();
 	}
+	
+	public void setAddress(String street,int housenumber, String city, String country,int postalcode) {
+		this.address.set(new Address(street, housenumber,city,country,postalcode));
+	}
+	
+	public ObjectProperty<Address> addressProperty() {
+		return address;		
+	}
+	
+	public ObservableList<String> getTelephoneNumbers() {
+		return telephoneNumbers.get();
+	}
+	
+	public void addTelephoneNumbers(String number) {
+		this.telephoneNumbers.add(number);
+	}
+	
+	public ListProperty telephoneNumberProperty() {
+		return telephoneNumbers;		
+	}
+	
+	public ObservableList<Contact> getContacts() {
+		return contacts.get();
+	}
+	
+	public void addContact(String email,String firstname, String surname) {
+		this.contacts.add(new Contact(email,firstname,surname));
+	}
+	
+	public SimpleListProperty<Contact> contactProperty() {
+		return contacts;		
+	}
+	
+	public Date getDateCreated() {
+		return dateCreated.get();
+	}
+	
+	public void setDateCreated(Date date) {
+		this.dateCreated.set(date);
+	}
+	
+	public SimpleObjectProperty<Date> dateCreatedProperty() {
+		return dateCreated;		
+	}
+	
+	
 
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-	
-	public ArrayList<Contact> getContacts(){
-		return this.contacts;
-	}
-	
-	public ArrayList<String> getTelephoneNumbers(){
-		return this.telephoneNumbers;
-	}
+
+
 }
